dd93328caafca4f37804dd68e9e926080d705a82
Bernardo De Oliveira <bernerd2120@gmail.com>
2025-12-07 06:11:25 -0300

    Primeiro código relacionado a hardware

---
+++ main_a.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(){
+    
+    int binaryNumber = 0b101;
+
+    int binaryGenerator = rand() % 100;
+
+    if(binaryGenerator)
+
+
+
+
+
+
+
+}
